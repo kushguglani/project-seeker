afce2021f188d9fa07a358b90ab7b28f160eba42
Kush Guglani <kush.guglani@nagarro.com>
2020-09-07 15:55:38 +0530

    heroku logcheck

--- helpers/db.js
+++ helpers/db.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+console.log(process.env.MONGODB_URI);
+console.log(process.env.NODE_ENV);
 const connectionOptions = { useCreateIndex: true, useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false };
 mongoose.connect(process.env.MONGODB_URI, connectionOptions);
 mongoose.Promise = global.Promise;
